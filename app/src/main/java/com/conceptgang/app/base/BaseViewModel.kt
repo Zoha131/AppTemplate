9eec2f7fb2cf44e2956dce3f9816eb9109de548f
zoha131 <abirhasanzoha@gmail.com>
2020-04-27 17:13:23 +0600

    roll back to default state

--- app/src/main/java/com/conceptgang/app/base/BaseViewModel.kt
+++ app/src/main/java/com/conceptgang/app/base/BaseViewModel.kt
@@ -4,5 +4,4 @@ import com.airbnb.mvrx.BaseMvRxViewModel
 import com.airbnb.mvrx.BuildConfig
 import com.airbnb.mvrx.MvRxState
 
-
 abstract class BaseViewModel<S: MvRxState>(state: S) : BaseMvRxViewModel<S>(state, BuildConfig.DEBUG)
